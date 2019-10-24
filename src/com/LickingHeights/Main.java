a09f7d5b17bf7591373d1bf56b36bb999d20678f
Camron Caldwell <camron.caldwell.2022@student.lhschools.org>
2019-10-24 10:07:51 -0400

    added the 1-100

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,18 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int pickedNumber;
+
+
+
+
+        for(int i = 0; i <20; i++) {
+            pickedNumber = (int) (Math.random()*100);
+            System.out.println(pickedNumber);
+        }
+
+    }
+}
